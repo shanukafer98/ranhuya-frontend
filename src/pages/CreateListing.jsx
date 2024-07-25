9126dde24898d26824cf16b1e73a3544e9881b6c
shanukafer98 <shanukafer98@gmail.com>
2024-07-25 15:48:00 +0530

    chore: Update fetch URL in CreateListing.jsx

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -184,7 +184,7 @@ useEffect(() => {
       
       console.log("Submitting form data:", formData);
 
-      const res = await fetch(`${url}/api/listing/create`, {
+      const res = await fetch("http://13.49.80.197:3000/api/listing/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
