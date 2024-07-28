a4c1bd1e948b796620e39e04ffa939e6d2ed476e
shanukafer98 <shanukafer98@gmail.com>
2024-07-28 13:54:05 +0530

    chore: Update link color for Home page in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
         </Link>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 text-lg font-bold    hover:text-slate-300'>
+            <li className='hidden sm:inline text-blue-600 text-lg font-bold    hover:text-slate-300'>
               Home
             </li>
           </Link>
